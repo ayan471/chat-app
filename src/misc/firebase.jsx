0b6957793e7d724b9cfed7ea84745c4d74a4a2e3
ayan471 <pakhiraayan@gmail.com>
2022-12-27 17:49:09 +0530

    edit signin page and signed in with facebook/google

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,8 +1,13 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyCQyAwWWF22M9uq5NGF7T6VJf3vUF0Efl4',
   authDomain: 'chat-web-app-40e34.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-40e34-default-rtdb.asia-southeast1.firebasedatabase.app',
+
   projectId: 'chat-web-app-40e34',
   storageBucket: 'chat-web-app-40e34.appspot.com',
   messagingSenderId: '1021553549665',
@@ -10,3 +15,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
