1c485f9cea0243db0a3324fd02445f9fee959b8b
ayan471 <pakhiraayan@gmail.com>
2022-12-31 13:03:04 +0530

    created layout for chat page

---
+++ src/components/chat-window/bottom/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bottom = () => {
+  return <div>hello</div>;
+};
+
+export default Bottom;
---
+++ src/components/chat-window/messages/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Messages = () => {
+  return <div>hi</div>;
+};
+
+export default Messages;
