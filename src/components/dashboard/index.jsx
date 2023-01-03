acf5bec962187e2371102d092a01a1ac6ecaf734
ayan471 <pakhiraayan@gmail.com>
2023-01-03 12:39:30 +0530

    profile avatar not updating properly

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -5,17 +5,22 @@ import { database } from '../../misc/firebase';
 import AvatarUploadBtn from './AvatarUploadBtn';
 import EditableInput from './EditableInput';
 import ProviderBlock from './ProviderBlock';
+import { getUserUpdates } from '../../misc/helper';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
-
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+      console.log('updates', updates);
+      await database.ref().update(updates);
+
       Alert.success('Nickname has been updated', 4000);
     } catch (err) {
       Alert.error(err.message, 4000);
