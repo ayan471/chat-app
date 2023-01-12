17687b910145c023936a32e34f6a2b9b97f6b7e6
ayan471 <pakhiraayan@gmail.com>
2023-01-12 14:27:27 +0530

    displayed audio messages and fix file deletion when message is being deleted

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-param-reassign */
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { auth, database } from '../../../misc/firebase';
+import { auth, database, storage } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helper';
 import MessageItem from './MessageItem';
 
@@ -74,7 +75,7 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId, file) => {
       // eslint-disable-next-line no-alert
       if (!window.confirm('Delete the message?')) {
         return;
@@ -97,7 +98,15 @@ const Messages = () => {
         await database.ref().update(updates);
         Alert.info('Message has been deleted');
       } catch (err) {
-        Alert.error(err.message);
+        return Alert.error(err.message);
+      }
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message);
+        }
       }
     },
     [chatId, messages]
